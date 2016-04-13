22638b999b13ed9e03b2b64e681591c29718293a
Adriele <adrielegf@users.noreply.github.com>
2016-04-13 13:44:31 -0300

    Added files via upload

---
+++ Projeto_Dimmer.ino
@@ -0,0 +1,17 @@
+const int PinoPotenciometro = 0;     
+const int Led = 11;       
+int ValorPot = 0;   
+int pwm = 0;  
+
+
+void setup() {   
+  pinMode(Led, OUTPUT); 
+  }  
+
+  
+void loop() { 
+  //digitalWrite(Led, HIGH);  
+  ValorPot = analogRead(PinoPotenciometro);       
+  pwm = map(ValorPot, 0, 1023, 0, 255);   
+  analogWrite(Led, pwm);                  
+  }
