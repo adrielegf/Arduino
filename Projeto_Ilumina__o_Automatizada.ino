a4bc1e6ef6b712f3fca1ec36f72810238dda7a60
Adriele <adrielegf@users.noreply.github.com>
2016-04-13 13:46:01 -0300

    Update Projeto_Ilumina__o_Automatizada.ino

--- Projeto_Ilumina__o_Automatizada.ino
+++ Projeto_Ilumina__o_Automatizada.ino
@@ -5,20 +5,20 @@ int ldrPin = 0; //LDR no pino analígico 0
 int ldrValor = 0; //Valor lido do LDR
  
 void setup() {
- pinMode(ledPin,OUTPUT); //define a porta 7 como saída
- Serial.begin(9600); //Inicia a comunicação serial
+ pinMode(ledPin,OUTPUT); 
+ Serial.begin(9600); 
 }
  
 void loop() {
- ///ler o valor do LDR
- ldrValor = analogRead(ldrPin); //O valor lido será entre 0 e 1023
  
- //se o valor lido for maior que 500, liga o led
- if (ldrValor>= 800) digitalWrite(ledPin,HIGH);
- // senão, apaga o led
+ ldrValor = analogRead(ldrPin);
+ 
+ 
+ if (ldrValor>= 400) digitalWrite(ledPin,HIGH);
+
  else digitalWrite(ledPin,LOW);
  
- //imprime o valor lido do LDR no monitor serial
+ 
  Serial.println(ldrValor);
  delay(100);
 }
